8f021353007ab969b71d47c6d757c24d1237a77e
ZT97-cell <93303439+ZT97-cell@users.noreply.github.com>
2023-08-03 15:28:30 -0500

    Add files via upload

---
+++ Module 10/whatabook_book.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: whatabook
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `book_id` int NOT NULL AUTO_INCREMENT,
+  `book_name` varchar(200) NOT NULL,
+  `author` varchar(200) NOT NULL,
+  `details` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'Watchmen','Alvin Moore',NULL),(2,'The Old Man and the Sea','Ernest Hemmingway',NULL),(3,'The Invisible Man','H.G. Wells',NULL),(4,'The Count of Monte Cristo','Alexandre Dumas',NULL),(5,'The Great Gatsby','F.Scott Fitzgerald',NULL),(6,'Journey to the Center of the Earth','Jules Verne',NULL),(7,'Slaughter House 5','Kurt Vonnegut',NULL),(8,'Mother Night','Kurt Vonnegut',NULL),(9,'The Sun Also Rises','Ernest Hemmingway',NULL),(10,'For Whom the Bell Tolls','Ernest Hemmingway',NULL);
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-03 12:20:26
---
+++ Module 10/whatabook_init.sql
@@ -0,0 +1,122 @@
+/*
+Title: whatabook_init.sql
+Author: Zachary Tharp
+Date: August 3, 2023
+Description: The creation of the whatabook database, user, and tables. The insertion of values into those tables
+/*
+
+-- creating whatabook databse
+CREATE DATABASE whatabook;
+
+-- creating the whatabook_user
+CREATE USER 'whatabook_user'@'locahlhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+
+-- granting priveleges to whatabook_user
+GRANT ALL PRIVILEGES ON whatabook.* TO 'whatabook_user'@'localhost';
+
+-- creating the user table
+CREATE TABLE user (
+	user_id INT  NOT NULL  AUTO_INCREMENT,
+    first_name  INT  NOT NULL,
+    last_name  INT  NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+-- creating the book table
+CREATE TABLE book (
+	book_id  INT  NOT NULL  AUTO_INCREMENT,
+    book_name  VARCHAR(200)  NOT NULL,
+    author  VARCHAR(200)  NOT NULL,
+    details  VARCHAR(500),
+    PRIMARY KEY (book_id)
+);
+
+-- creating the store table
+CREATE TABLE store (
+	store_id  INT  NOT NULL  AUTO_INCREMENT,
+    locale  VARCHAR(500)  NOT NULL,
+    PRIMARY KEY (store_id)
+);
+
+-- creating the wishlist table 
+CREATE TABLE wishlist (
+	wishlist_id  INT  NOT NULL  AUTO_INCREMENT,
+	user_id  INT  NOT NULL,
+	book_id  INT  NOT NULL,
+	PRIMARY KEY (wishlist_id),
+	CONSTRAINT fk_user
+	FOREIGN KEY (user_id)
+		REFERENCES user(user_id),
+	CONSTRAINT fk_book
+	FOREIGN KEY (book_id)
+		REFERENCES book(book_id)
+);
+
+-- inserting the store records
+INSERT INTO store(locale)
+	VALUES ('1912 S Park St. Pocahontas,AR 72455');
+    
+-- inserting book records
+INSERT INTO book (book_name, author)
+	VALUES ('Watchmen', 'ALvin Moore');
+    
+INSERT INTO book (book_name, author)
+	VALUES ('The Old Man and the Sea', 'Ernest Hemmingway');
+
+INSERT INTO book (book_name, author)
+	VALUES ('The Invisibe Man', 'H.G. Wells');
+    
+INSERT INTO book (book_name, author)
+	VALUES ('The Count of Monte Cristo', 'Alexandre Dumas');
+    
+INSERT INTO book (book_name, author)
+	VALUES ('The Great Gatsby', 'F.Scott Fitzgerald');
+    
+INSERT INTO book (book_name, author)
+	VALUES ('Journey to the Center of the Earth', 'Jules Verne');
+    
+INSERT INTO book (book_name, author)
+	VALUES ('Slaughter House 5', ' Kurt Vonnegut');
+    
+INSERT INTO book (book_name, author)
+	VALUES ('Mother Night', 'Kurt Vonnegut');
+
+INSERT INTO book (book_name, author)
+	VALUES ('The Sun Also Rises', 'Ernest Hemmingway');
+    
+INSERT INTO book (book_name, author)
+	VALUES ('For Whom the Bell Tolls', 'Ernest Hemmingway');
+    
+-- inserting into user records
+INSERT INTO user (first_name, last_name)
+	VALUES ('Aaron', 'Rodgers');
+    
+INSERT INTO user (first_name, last_name)
+	VALUES ('Kirk', 'Cousins');
+    
+INSERT INTO user (first_name, last_name)
+	VALUES ('Josh', 'Allen');
+    
+-- inserting into wishlist records
+INSERT INTO wishlist (user_id, book_id)
+	VALUES (
+		(SELECT user_id FROM user WHERE first_name = 'Aaron'),
+        (SELECET book_id FROM book WHERE book_name = 'Mother Night')
+	);
+
+INSERT INTO wishlist (user_id, book_id)
+	VALUES (
+		(SELECT user_id FROM user WHERE first_name = 'Kirk'),
+        (SELECT book_id FROM book WHERE book_name = 'Slaughter House 5')
+	);
+    
+INSERT INTO wishlist (user_id, book_id)
+	VALUES (
+		(SELECT user_id FROM user WHERE first_name = 'Josh'),
+        (SELECT book_id FROM book WHERE book_name = 'The Sun Also Rises)
+	);
+        
+
+
+
+
